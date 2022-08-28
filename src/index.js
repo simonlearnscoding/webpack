fb34917a3176f51166c613f6b0a50973a9e99251
simonlearnscoding <simon.muscas@gmail.com>
2022-08-28 19:01:12 +0200

    done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import _ from 'lodash';
-import myName from './myName';
+import printMe from "./print";
+
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
 
     //lodash now imported
-    element.textContent = myName('Simon')
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe();
+
+    element.appendChild(btn);
     return element
 }
 
